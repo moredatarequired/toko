ddfa7cd7075826c4eb137dc7ab934f51afd7c9f0
Hugh Wimberly <hugh@fractional.ai>
2025-10-27 20:01:48 -0700

    ci: setup automatic pypi release

---
+++ tests/smoke_test.py
@@ -0,0 +1,19 @@
+"""Check that basic features work.
+
+Catch cases where e.g. files are missing so the import doesn't work.
+"""
+
+import toko
+from toko.counter import count_tokens
+from toko.models import list_models
+
+assert toko.__version__ is not None
+assert toko.__version__ != "0.0.0"
+assert count_tokens("The most basic of smoke tests.", model="gpt-5") == 7
+
+models_by_provider = list_models()
+assert len(models_by_provider) > 4
+assert len(models_by_provider["anthropic"]) > 5
+assert len(models_by_provider["openai"]) > 5
+
+print("Smoke test succeeded")
